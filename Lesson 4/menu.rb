3120aefad047e4df93d5089f904075cfcb5b6b62
OlyaPash <94684352+OlyaPash@users.noreply.github.com>
2022-04-27 22:18:21 +0500

    Update menu.rb

--- Lesson 4/menu.rb
+++ Lesson 4/menu.rb
@@ -3,8 +3,8 @@ require_relative 'route'
 require_relative 'train'
 require_relative 'passenger_train'
 require_relative 'cargo_train'
-require_relative 'wagon_passenger'
-require_relative 'wagon_cargo'
+require_relative 'passenger_wagon'
+require_relative 'cargo_wagon'
 require_relative 'main'
 
 interface = Main.new
